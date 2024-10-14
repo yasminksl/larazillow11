ed29d23fa4fbe1735114e6c0d22bfc49eb562b46
yasminksl <yasminksl321@gmail.com>
2024-10-14 08:48:21 -0300

    Default layouts
    - Colocar um Layout pertinente em todas as páginas, se precisar colocar um layout específico na página, adicionar um script com export default definindo o Layout.

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -11,13 +11,4 @@
 const props = defineProps({
     message: String,
 })
-
-
-</script>
-
-<script>
-import MainLayout from '../../Layouts/MainLayout.vue';
-export default {
-    layout: MainLayout,
-}
 </script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -3,10 +3,3 @@
             Show
         </div>
 </template>
-
-<script>
-import MainLayout from '../../Layouts/MainLayout.vue';
-export default {
-    layout: MainLayout,
-}
-</script>
