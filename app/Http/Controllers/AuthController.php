c3a68ff54da6cb0f757a7ce46646b2e1b4db4c59
yasminksl <yasminksl321@gmail.com>
2024-10-17 14:39:10 -0300

    Página de Login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,9 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function create() {}
+    public function create() {
+        return inertia('Auth/Login');
+    }
 
     public function store() {}
 
