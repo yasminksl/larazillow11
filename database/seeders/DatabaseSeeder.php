728c92e2cf4a427c16655bbe54e6bc979bccc710
yasminksl <yasminksl321@gmail.com>
2024-10-21 08:29:05 -0300

    Relação de Um para Muitos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,8 @@ class DatabaseSeeder extends Seeder
             'email' => 'test@example.com',
         ]);
 
-        Listing::factory(20)->create();
+        Listing::factory(20)->create([
+            'by_user_id' => 1
+        ]);
     }
 }
