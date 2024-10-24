33b3aa8173b4be3ac7b3ac003409f48655c5d479
yasminksl <yasminksl321@gmail.com>
2024-10-24 11:31:38 -0300

    Upload Multiple Images

--- app/Http/Controllers/RealtorListingImageController.php
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Listing;
+use App\Models\ListingImage;
 use Illuminate\Http\Request;
 
 class RealtorListingImageController extends Controller
@@ -10,14 +11,23 @@ class RealtorListingImageController extends Controller
     public function create(Listing $listing)
     {
         return inertia(
-            'Realtor/ListingImage/Create', [
+            'Realtor/ListingImage/Create',
+            [
                 'listing' => $listing
             ]
-            );
+        );
     }
 
     public function store(Listing $listing, Request $request)
     {
-        dd('Works!');
+        if ($request->hasFile('images')) {
+            foreach ($request->file('images') as $file) {
+                $path = $file->store('images', 'public');
+                $listing->images()->save(new ListingImage([
+                    'filename' => $path
+                ]));
+            }
+        }
+        return redirect()->back()->with('success', 'Images uploaded!');
     }
 }
--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -1,6 +1,7 @@
 <template>
-    <form method="post" :action="route('realtor.listing.image.store', { listing: listing.id })">
-        <input type="file" multiple name="files">
+    <form enctype="multipart/form-data" method="POST"
+        :action="route('realtor.listing.image.store', { listing: listing.id })">
+        <input type="file" multiple name="images[]" />
         <button type="submit">Send</button>
     </form>
 </template>
