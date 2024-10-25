aa2d33dd5ba79cca8516c509e529aab9bf3b7d2b
yasminksl <yasminksl321@gmail.com>
2024-10-25 13:46:17 -0300

    Delete images

--- app/Http/Controllers/RealtorListingImageController.php
+++ app/Http/Controllers/RealtorListingImageController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\Models\Listing;
 use App\Models\ListingImage;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class RealtorListingImageController extends Controller
 {
     public function create(Listing $listing)
     {
         $listing->load(['images']);
-        
+
         return inertia(
             'Realtor/ListingImage/Create',
             [
@@ -31,5 +32,13 @@ class RealtorListingImageController extends Controller
             }
         }
         return redirect()->back()->with('success', 'Images uploaded!');
+    }
+
+    public function destroy($listing, ListingImage $image)
+    {
+        Storage::disk('public')->delete($image->filename);
+        $image->delete();
+
+        return redirect()->back()->with('success', 'Image was deleted!');
     }
 }
--- resources/js/Pages/Realtor/ListingImage/Create.vue
+++ resources/js/Pages/Realtor/ListingImage/Create.vue
@@ -3,7 +3,9 @@
         <template #header>Upload New Images</template>
         <form @submit.prevent="upload">
             <section class="flex items-center gap-2 my-4">
-                <input class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-50 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4" type="file" multiple @input="addFiles" />
+                <input
+                    class="border rounded-md file:px-4 file:py-2 border-gray-200 dark:border-gray-50 file:text-gray-700 file:dark:text-gray-400 file:border-0 file:bg-gray-100 file:dark:bg-gray-800 file:font-medium file:hover:bg-gray-200 file:dark:hover:bg-gray-700 file:hover:cursor-pointer file:mr-4"
+                    type="file" multiple @input="addFiles" />
                 <button type="submit" class="btn-outline disabled:opacity-25 disabled:cursor-not-allowed"
                     :disabled="!canUpload">Upload</button>
                 <button type="reset" class="btn-outline" @click="reset">Reset</button>
@@ -14,8 +16,11 @@
     <Box v-if="listing.images.length" class="mt-4">
         <template #header>Current Listing Images</template>
         <section class="mt-4 grid grid-cols-3 gap-4">
-            <div v-for="image in listing.images" :key="image.id">
+            <div v-for="image in listing.images" :key="image.id" class="flex flex-col justify-between">
                 <img :src="image.src" class="rounded-md" />
+                <Link :href="route('realtor.listing.image.destroy', {listing: props.listing.id, image: image.id})" method="delete" as="button" class="mt-2 btn-outline text-xs">
+                    Delete
+                </Link>
             </div>
         </section>
     </Box>
@@ -24,8 +29,7 @@
 <script setup>
 import { computed } from 'vue';
 import Box from '@/Components/UI/Box.vue'
-import { useForm } from '@inertiajs/vue3';
-import { router } from '@inertiajs/vue3';
+import { useForm, router, Link } from '@inertiajs/vue3'
 import NProgress from 'nprogress'
 
 const props = defineProps({
@@ -33,9 +37,9 @@ const props = defineProps({
 })
 
 router.on('progress', (event) => {
-  if (event.detail.progress.percentage) {
-    NProgress.set((event.detail.progress.percentage / 100) * 0.9)
-  }
+    if (event.detail.progress.percentage) {
+        NProgress.set((event.detail.progress.percentage / 100) * 0.9)
+    }
 })
 
 const form = useForm({
