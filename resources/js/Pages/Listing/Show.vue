af547c8efb16009c98615a61047d9d5af6bb4b50
yasminksl <yasminksl321@gmail.com>
2024-10-25 13:49:43 -0300

    Mostrando images no Show Listing

--- resources/js/Pages/Listing/Show.vue
+++ resources/js/Pages/Listing/Show.vue
@@ -1,7 +1,10 @@
 <template>
     <div class="flex flex-col-reverse md:grid md:grid-cols-12 gap-4">
         <Box class="md:col-span-7 flex items-center w-full">
-            <div class="w-full text-center font-medium text-gray-500">No images</div>
+            <div v-if="listing.images.length" class="grid grid-cols-2 gap-1">
+                <img v-for="image in listing.images" :key="image.id" :src="image.src">
+            </div>
+            <div v-else class="w-full text-center font-medium text-gray-500">No images</div>
         </Box>
         <div class="md:col-span-5 flex flex-col gap-4">
             <Box>
@@ -36,19 +39,19 @@
                         <div class="flex justify-between">
                             <div>Total paid</div>
                             <div>
-                                <Price :price="totalPaid" class="font-medium"/>
+                                <Price :price="totalPaid" class="font-medium" />
                             </div>
                         </div>
                         <div class="flex justify-between">
                             <div>Principal paid</div>
                             <div>
-                                <Price :price="listing.price" class="font-medium"/>
+                                <Price :price="listing.price" class="font-medium" />
                             </div>
                         </div>
                         <div class="flex justify-between">
                             <div>Interest paid</div>
                             <div>
-                                <Price :price="totalInterest" class="font-medium"/>
+                                <Price :price="totalInterest" class="font-medium" />
                             </div>
                         </div>
                     </div>
