34134595aed7ec21a89ea79b100785158c83c934
yasminksl <yasminksl321@gmail.com>
2024-11-01 11:19:03 -0300

    Sino e contagem de notificações

--- resources/js/Layouts/MainLayout.vue
+++ resources/js/Layouts/MainLayout.vue
@@ -9,6 +9,15 @@
           <Link href="/">LaraZillow</Link>
         </div>
         <div v-if="user" class="flex items-center gap-4">
+          <div class="text-gray-500 relative pr-2 py-2 text-lg">
+            🔔
+            <div
+              class="absolute right-0 top-0 w-5 h-5 bg-red-700 dark:bg-red-400 text-white font-medium border border-white dark:border-gray-900 rounded-full text-xs text-center"
+            >
+              {{ notificationCount }}
+            </div>
+          </div>
+
           <Link :href="route('realtor.listing.index')" class="text-sm text-gray-500">{{ user.name }}</Link>
           <Link :href="route('realtor.listing.create')" class="btn-primary">
             + New Listing
@@ -43,5 +52,6 @@ import { Link, usePage } from '@inertiajs/vue3'
 const page = usePage()
 const flashSuccess = computed(() => page.props.flash.success)
 const user = computed(() => page.props.user)
+const notificationCount = computed(() => Math.min(page.props.user.notificationCount, 9))
 
 </script>
