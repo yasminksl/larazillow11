3e2e6d246e4338b2f4c6988b55db6ddcfa180b86
yasminksl <yasminksl321@gmail.com>
2024-11-04 09:03:07 -0300

    Exigindo usuário verificado para acessar algumas rotas

---
+++ resources/js/Pages/Auth/VerifyEmail.vue
@@ -0,0 +1,7 @@
+<template>
+    You need be verified first to see this page!
+</template>
+
+<script>
+
+</script>
--- routes/web.php
+++ routes/web.php
@@ -33,12 +33,16 @@ Route::get('/login', [AuthController::class, 'create'])->name('login');
 Route::post('/login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('/logout', [AuthController::class, 'destroy'])->name('logout');
 
+Route::get('/email/verify', function () {
+    return inertia('Auth/VerifyEmail');
+})->middleware('auth')->name('verification.notice');
+
 Route::resource('user-account', UserAccountController::class)
     ->only(['create', 'store']);
 
 Route::prefix('realtor')
     ->name('realtor.')
-    ->middleware('auth')
+    ->middleware(['auth', 'verified'])
     ->group(function () {
         Route::name('listing.restore')->put('listing/{listing}/restore', [RealtorListingController::class, 'restore'])->withTrashed();
 
