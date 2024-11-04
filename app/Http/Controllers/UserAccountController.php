a20654cc2f5634875d9482fb13359ef9c6e8cbf8
yasminksl <yasminksl321@gmail.com>
2024-11-04 08:52:50 -0300

    Iniciando processo de verificação de e-mail quando registrar um novo usuário

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\User;
+use Illuminate\Auth\Events\Registered;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -23,6 +24,7 @@ class UserAccountController extends Controller
 
         $user->save();
         Auth::login($user);
+        event(new Registered($user));
 
         return redirect()->route('listing.index')
             ->with('success', 'Account created');
